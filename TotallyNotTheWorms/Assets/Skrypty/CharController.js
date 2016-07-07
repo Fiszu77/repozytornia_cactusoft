e10561b13632ec837e4a3b2e65b5453f11dbdc8f
Cactusoft Entertainment <bukaj.i.kaktus@gmail.com>
2016-07-07 23:56:11 +0200

    Pierwsze kroki

---
+++ TotallyNotTheWorms/Assets/Skrypty/CharController.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+
+function Start () {
+
+}
+
+function Update () {
+	//GetComponent.<Rigidbody2D>().AddForce(Vector2.down*10);
+	if(Input.GetKey("a")) {
+	 //GetComponent.<Rigidbody2D>().AddForce(Vector2.left*10);
+	 transform.Translate(-0.1,0,0*Time.deltaTime);
+	 GetComponent.<SpriteRenderer>().flipX=false;
+	}
+	if(Input.GetKey("d")) {
+	 //GetComponent.<Rigidbody2D>().AddForce(Vector2.right*10);
+	 transform.Translate(0.1,0,0*Time.deltaTime);
+	 GetComponent.<SpriteRenderer>().flipX=true;
+	}
+	if(Input.GetKeyDown("space")) {
+	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*10000);
+	}
+}
