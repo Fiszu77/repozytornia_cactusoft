1dd312ac1e5ee2f0f520a62dd02fef23c87d0570
Cactusoft Entertainment <bukaj.i.kaktus@gmail.com>
2016-07-09 01:16:42 +0200

    Sterowanie ogarnięte! (możliwe, że trza skalibrować)

--- TotallyNotTheWorms/Assets/Skrypty/CharController.js
+++ TotallyNotTheWorms/Assets/Skrypty/CharController.js
@@ -1,49 +1,76 @@
 ﻿#pragma strict
 var top_left : Transform;
 var bottom_right : Transform;
-var grounded : boolean;
+
 var ground_layers :LayerMask;
-var dir: boolean;// true =right, false = left; 
+
+var cantJumpTimer : float;
+var jump2Timer : float;
+var speedOfMove : float;
+
+var jumpForceUp : int;
+var jumpForceSide : int;
+var jumpForceUp2 : int;
+var jumpForceSide2 : int;
+
+var grounded : boolean;
+var isJump2 : boolean;
+
 var pointsArray:Vector2[];
 
 function Start () {
 	GetComponent.<EdgeCollider2D>().points=pointsArray;
-	dir = false;
 }
 
 function FixedUpdate () {
 	grounded = Physics2D.OverlapArea(top_left.position, bottom_right.position, ground_layers);
-	//GetComponent.<Rigidbody2D>().AddForce(Vector2.down*10);
-	if(Input.GetKey("a")) {
+	if(grounded) {
+	 cantJumpTimer+=Time.deltaTime;
+	}
+	else {
+	 cantJumpTimer=0;
+	}
+	if(isJump2) {
+	 jump2Timer+=Time.deltaTime;
+	}
+	if(Input.GetKey(KeyCode.A) && grounded) {
 	 GetComponent.<SpriteRenderer>().flipX=false;
-	 dir = false;
 	 //GetComponent.<Rigidbody2D>().AddForce(Vector2.left*10);
-	 if (grounded)
-	  transform.Translate(-0.1,0,0*Time.deltaTime);
-	  else
-	  transform.Translate(-0.25,0,0*Time.deltaTime);
-	  }
-	if(Input.GetKey("d")) {
+	 transform.Translate(-speedOfMove,0,0*Time.deltaTime);
+	}
+	if(Input.GetKey(KeyCode.D) && grounded) {
 	 GetComponent.<SpriteRenderer>().flipX=true;
-	 dir = true;
 	 //GetComponent.<Rigidbody2D>().AddForce(Vector2.right*10);
-	 if (grounded)
-	  transform.Translate(0.1,0,0*Time.deltaTime);
-	  else
-	  transform.Translate(0.25,0,0*Time.deltaTime);
+	 transform.Translate(speedOfMove,0,0*Time.deltaTime);
 	}
-	if(Input.GetKeyDown("space") && grounded == true){
-	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*1100);
-	 	
+	if(Input.GetKeyDown(KeyCode.Space) && grounded && cantJumpTimer>=1){
+	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*jumpForceUp);
+	 cantJumpTimer=0;
+	 isJump2=false;
+	 jump2Timer=0;
+	 if(!GetComponent.<SpriteRenderer>().flipX) {
+	  GetComponent.<Rigidbody2D>().AddForce(Vector2.left*jumpForceSide);
+	 }
+	 else {
+	  GetComponent.<Rigidbody2D>().AddForce(Vector2.right*jumpForceSide);
+	 }
 	}
-
-
-	if(Input.GetKeyDown("backspace")&& grounded) {
-	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*1600);
-		if (!dir)
-			GetComponent.<Rigidbody2D>().AddForce(Vector2.right *220);
-		else
-			GetComponent.<Rigidbody2D>().AddForce(Vector2.left *220);
+	if(Input.GetKeyDown(KeyCode.LeftShift) && grounded && cantJumpTimer>=1) {
+	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*jumpForceUp2);
+	 isJump2=true;
+	 jump2Timer=0;
+	}
+	if(!GetComponent.<SpriteRenderer>().flipX) {
+	 if(jump2Timer>=0.4 && !grounded) {
+	  GetComponent.<Rigidbody2D>().AddForce(Vector2.right *jumpForceSide2);
+	  jump2Timer=0;
+	 }
+	}
+	else {
+	 if(jump2Timer>=0.4 && !grounded) {
+	  GetComponent.<Rigidbody2D>().AddForce(Vector2.left *jumpForceSide2);
+	  jump2Timer=0;
+	 }
 	}
 
 }
