2b71611352ed39d7487df3fc37ae4f5d865a6831
Kaktus <kaktus500@interia.eu>
2016-07-10 20:38:36 +0200

    Dodana pozycja myszki względem postaci

--- TotallyNotTheWorms/Assets/Skrypty/Shoot.js
+++ TotallyNotTheWorms/Assets/Skrypty/Shoot.js
@@ -1,6 +1,10 @@
 ﻿#pragma strict
 var instantiatepos : Transform;
 var bomb : Rigidbody2D;
+
+var mouseposx : float;
+var mouseposy : float;//pozycja myszki względem postaci
+ 
 function Start () {
 
 }
@@ -11,4 +15,7 @@ if(Input.GetKeyDown(KeyCode.E)){
 Instantiate(bomb,instantiatepos.position, Quaternion.identity);
 
 }
+
+mouseposx = (Screen.width*0.494-Input.mousePosition.x);
+mouseposy = (Screen.height*0.6875-Input.mousePosition.y);
 }
