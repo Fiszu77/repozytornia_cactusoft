c454363be0f7ac81203b2f6761b644719533faf3
Cactusoft Entertainment <bukaj.i.kaktus@gmail.com>
2016-07-12 23:48:30 +0200

    Sterowanie skokiem.

--- TotallyNotTheWorms/Assets/Skrypty/CharController.js
+++ TotallyNotTheWorms/Assets/Skrypty/CharController.js
@@ -7,6 +7,7 @@ var ground_layers :LayerMask;
 var cantJumpTimer : float;
 var jump2Timer : float;
 var speedOfMove : float;
+var DblClckTimer:float=0;
 
 var jumpForceUp : int;
 var jumpForceSide : int;
@@ -15,6 +16,7 @@ var jumpForceSide2 : int;
 
 var grounded : boolean;
 var isJump2 : boolean;
+var jump:boolean=false;
 
 var pointsArray:Vector2[];
 
@@ -23,54 +25,70 @@ function Start () {
 }
 
 function FixedUpdate () {
+//co oznacza "grounded"
 	grounded = Physics2D.OverlapArea(top_left.position, bottom_right.position, ground_layers);
+//niemożność skakania przez 1 sekundę
 	if(grounded) {
 	 cantJumpTimer+=Time.deltaTime;
 	}
 	else {
 	 cantJumpTimer=0;
 	}
+//licznik do drugiej części skoku do tyłu
 	if(isJump2) {
 	 jump2Timer+=Time.deltaTime;
 	}
+//ruch w lewo i prawo
 	if(Input.GetKey(KeyCode.A) && grounded) {
 	 GetComponent.<SpriteRenderer>().flipX=false;
-	 //GetComponent.<Rigidbody2D>().AddForce(Vector2.left*10);
 	 transform.Translate(-speedOfMove,0,0*Time.deltaTime);
 	}
 	if(Input.GetKey(KeyCode.D) && grounded) {
 	 GetComponent.<SpriteRenderer>().flipX=true;
-	 //GetComponent.<Rigidbody2D>().AddForce(Vector2.right*10);
 	 transform.Translate(speedOfMove,0,0*Time.deltaTime);
 	}
+//skok
 	if(Input.GetKeyDown(KeyCode.Space) && grounded && cantJumpTimer>=1){
-	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*jumpForceUp);
+	 DblClckTimer=0;
+	 jump=true;
 	 cantJumpTimer=0;
 	 isJump2=false;
 	 jump2Timer=0;
-	 if(!GetComponent.<SpriteRenderer>().flipX) {
+	}
+	if(DblClckTimer>0.2) {
+	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*jumpForceUp);
+	 if(!GetComponent.<SpriteRenderer>().flipX) { //jeśli jest odwrócony w lewo to skacz w lewo
 	  GetComponent.<Rigidbody2D>().AddForce(Vector2.left*jumpForceSide);
 	 }
-	 else {
+	 else { //jeśli nie to w prawo
 	  GetComponent.<Rigidbody2D>().AddForce(Vector2.right*jumpForceSide);
 	 }
+	 jump=false;
+	 DblClckTimer=0;
 	}
-	if(Input.GetKeyDown(KeyCode.LeftShift) && grounded && cantJumpTimer>=1) {
+	if(Input.GetKeyDown(KeyCode.Space) && DblClckTimer<=0.2 && DblClckTimer>0) {
 	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*jumpForceUp2);
+	 jump=false;
+	 DblClckTimer=0;
 	 isJump2=true;
 	 jump2Timer=0;
 	}
 	if(!GetComponent.<SpriteRenderer>().flipX) {
 	 if(jump2Timer>=0.4 && !grounded) {
 	  GetComponent.<Rigidbody2D>().AddForce(Vector2.right *jumpForceSide2);
+	  isJump2=false;
 	  jump2Timer=0;
 	 }
 	}
 	else {
 	 if(jump2Timer>=0.4 && !grounded) {
 	  GetComponent.<Rigidbody2D>().AddForce(Vector2.left *jumpForceSide2);
+	  isJump2=false;
 	  jump2Timer=0;
 	 }
+	}
+	if(jump) {
+	 DblClckTimer+=Time.deltaTime;
 	}
 }
 
