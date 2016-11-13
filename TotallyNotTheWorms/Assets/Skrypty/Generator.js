170debea31b2ce0415d590e003d6598da0fd8eca
Cactusoft Entertainment <bukaj.i.kaktus@gmail.com>
2016-11-14 00:05:15 +0100

    We have to build a generator and we're halfway to the end (but still they will pay for it!)

--- TotallyNotTheWorms/Assets/Skrypty/Generator.js
+++ TotallyNotTheWorms/Assets/Skrypty/Generator.js
@@ -15,18 +15,14 @@ var x:int;
 var i:int;
 var h:int;
 var n:int;
+var dOrNoD:int;
+var fOrFh:int;
 
 function Start () {
 	h=Random.Range(0,6);
 	n=Random.Range(0,2);
 	//(poziom -> h.n)
-	for (i=0; i<10; i++) {
-
-	if(h==0) {
-	  if(n==0) {
-	    Down();
-	  }
-	 }
+	for (i=0; i<20; i++) {
 
 	for (x=0; x<2; x++) {
 	 switch(h) {
@@ -34,13 +30,34 @@ function Start () {
 	   if(n==0) {//Lewa połowa atomu (poziom 0.0)
 	    print("0.0");
 	    GenAtomHlfLeft();
-	    Next();
-		h=0;
-	    n=1;
+	    dOrNoD=Random.Range(0,2);
+	    switch(dOrNoD) {//Lecimy w dół czy nie?
+	     case 0://Lecimy w dół
+	      GenUnder();
+	      Down();
+	      fOrFh=Random.Range(0,2);
+	      switch(fOrFh) {//Następnie 5 czy 4.1?
+	       case 0:
+		    h=5;
+	       break;
+	       case 1:
+	        h=4;
+	        n=0;
+	       break;
+	      }
+	     break;
+	     case 1://Nie lecimy w dół
+	      GenUnder();
+	      Next();
+	      h=0;
+	      n=1;
+	     break;
+	    }
 	   }
 	   if(n==1) {//Prawa połowa atomu (poziom 0.1)
 	    print("0.1");
 	    GenAtomHlfRight();
+	    GenUnder();
 	    Next();
 		h=Random.Range(0,2);
 	    n=0;
@@ -49,6 +66,7 @@ function Start () {
 	  case 1://Cały 1 atom (poziom 1)
 	   print("1");
 	   GenAtom();
+	   GenUnder();
 	   Next();
 	   h=Random.Range(0,3);
 	   if(h==0) {
@@ -63,6 +81,7 @@ function Start () {
 	    print("2.0");
 	    GenAtom();
 	    GenNxtAtomHlfLeft();
+	    GenUnder();
 	    Next();
 	    h=Random.Range(0,3);
 	    if(h==0) {
@@ -76,6 +95,7 @@ function Start () {
 	    print("2.1");
 	    GenAtom();
 	    GenNxtAtomHlfRight();
+	    GenUnder();
 	    Next();
 	    h=Random.Range(2,5);
 	    if(h==2) {
@@ -90,6 +110,7 @@ function Start () {
 	   print("3");
 	   GenAtom();
 	   GenNxtAtom();
+	   GenUnder();
 	   Next();
 	   h=Random.Range(2,5);
 	   if(h==2) {
@@ -105,6 +126,7 @@ function Start () {
 	    GenAtom();
 	    GenNxtAtom();
 	    GenNxtAtomHlfLeft();
+	    GenUnder();
 	    Next();
 	    h=Random.Range(2,5);
 	    if(h==2) {
@@ -119,6 +141,7 @@ function Start () {
 	    GenAtom();
 	    GenNxtAtom();
 	    GenNxtAtomHlfRight();
+	    GenUnder();
 	    Next();
 	    h=Random.Range(4,6);
 	    if(h==4) {
@@ -131,6 +154,7 @@ function Start () {
 	   GenAtom();
 	   GenNxtAtom();
 	   GenNxtAtom();
+	   GenUnder();
 	   Next();
 	   h=Random.Range(4,6);
 	   if(h==4) {
@@ -150,10 +174,10 @@ function GenAtom () {
 }
 
 function GenNxtAtom () {
-	atomRightTrans.position.y+=2.5;
-	atomBottomTrans.position.y+=2.5;
-	atomLeftTrans.position.y+=2.5;
-	atomTopTrans.position.y+=2.5;
+	atomRightTrans.localPosition.y+=2.5;
+	atomBottomTrans.localPosition.y+=2.5;
+	atomLeftTrans.localPosition.y+=2.5;
+	atomTopTrans.localPosition.y+=2.5;
 	Instantiate(atomPrefabLeft,atomRightTrans.position,atomRightTrans.rotation);
 	Instantiate(atomPrefabBottom,atomBottomTrans.position,atomBottomTrans.rotation);
 	Instantiate(atomPrefabRight,atomLeftTrans.position,atomLeftTrans.rotation);
@@ -185,19 +209,28 @@ function GenNxtAtomHlfRight () {
 }
 
 function Next () {
-	atomRightTrans.position.x+=2.5;
-	atomLeftTrans.position.x+=2.5;
-	atomTopTrans.position.x+=2.5;
-	atomBottomTrans.position.x+=2.5;
-	atomRightTrans.position.y=0;
-	atomLeftTrans.position.y=0;
-	atomTopTrans.position.y=0;
-	atomBottomTrans.position.y=0;
+	atomRightTrans.localPosition.y=0;
+	atomLeftTrans.localPosition.y=0;
+	atomTopTrans.localPosition.y=0;
+	atomBottomTrans.localPosition.y=0;
+	groundTrans.position.x+=2.5;
 }
 
 function Down () {
-	groundTrans.position.y-=2.5;
-	groundTrans.position.y-=2.5;
-	groundTrans.position.y-=2.5;
-	groundTrans.position.y-=2.5;
+	atomRightTrans.localPosition.y=0;
+	atomLeftTrans.localPosition.y=0;
+	atomTopTrans.localPosition.y=0;
+	atomBottomTrans.localPosition.y=0;
+	groundTrans.position.y-=7.5;
+	groundTrans.position.x+=2.5;
+}
+function GenUnder () {
+	var m:int;
+	groundTrans.position.y-=7.5;
+	GenAtom();
+	for(m=0; m<21; m++) {
+	GenNxtAtom();
+	}
+	groundTrans.position.y+=7.5;
+		
 }
