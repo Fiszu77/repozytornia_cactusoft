bee3da86255cbe73b8dfcc1c7b05c9aa71f1bbdd
Cactusoft Entertainment <bukaj.i.kaktus@gmail.com>
2016-11-22 22:32:39 +0100

    Lekka zmiana sterowania skakania

--- TotallyNotTheWorms/Assets/Skrypty/CharController.js
+++ TotallyNotTheWorms/Assets/Skrypty/CharController.js
@@ -7,9 +7,8 @@ var legs : GameObject[];
 var ground_layers :LayerMask;
 
 var cantJumpTimer : float;
-var jump2Timer : float;
 var speedOfMove : float;
-var DblClckTimer:float=0;
+var jumpTimer : float;
 
 var jumpForceUp : int;
 var jumpForceSide : int;
@@ -36,9 +35,9 @@ function FixedUpdate () {
 	else {
 	 cantJumpTimer=0;
 	}
-//licznik do drugiej części skoku do tyłu
-	if(isJump2) {
-	 jump2Timer+=Time.deltaTime;
+//licznik skoku do tyłu
+	if(isJump2==true) {
+	 jumpTimer+=Time.deltaTime;
 	}
 //ruch w lewo i prawo
 	if(Input.GetKey(KeyCode.A) && grounded) {
@@ -60,13 +59,8 @@ function FixedUpdate () {
 	}
 //skok
 	if(Input.GetKeyDown(KeyCode.Space) && grounded && cantJumpTimer>=1){
-	 DblClckTimer=0;
 	 jump=true;
 	 cantJumpTimer=0;
-	 isJump2=false;
-	 jump2Timer=0;
-	}
-	if(DblClckTimer>0.2) {
 	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*jumpForceUp);
 	 if(!GetComponent.<SpriteRenderer>().flipX) { //jeśli jest odwrócony w lewo to skacz w lewo
 	  GetComponent.<Rigidbody2D>().AddForce(Vector2.left*jumpForceSide);
@@ -74,32 +68,31 @@ function FixedUpdate () {
 	 else { //jeśli nie to w prawo
 	  GetComponent.<Rigidbody2D>().AddForce(Vector2.right*jumpForceSide);
 	 }
-	 jump=false;
-	 DblClckTimer=0;
 	}
-	if(Input.GetKeyDown(KeyCode.Space) && DblClckTimer<=0.2 && DblClckTimer>0 && jump==true) {
-	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*jumpForceUp2);
-	 jump=false;
-	 DblClckTimer=0;
+	if(Input.GetKeyDown(KeyCode.LeftAlt) && grounded && cantJumpTimer>=1) {
+	 jump=true;
+	 cantJumpTimer=0;
 	 isJump2=true;
-	 jump2Timer=0;
+	 GetComponent.<Rigidbody2D>().AddForce(Vector2.up*jumpForceUp2);
 	}
-	if(!GetComponent.<SpriteRenderer>().flipX) {
-	 if(jump2Timer>=0.4 && !grounded) {
-	  GetComponent.<Rigidbody2D>().AddForce(Vector2.right *jumpForceSide2);
-	  isJump2=false;
-	  jump2Timer=0;
-	 }
+	 if(jumpTimer>=0.8) {
+	  if(!GetComponent.<SpriteRenderer>().flipX) { //jeśli jest odwrócony w lewo to skacz w prawo
+	   GetComponent.<Rigidbody2D>().AddForce(Vector2.right*jumpForceSide2);
+	   jumpTimer=0;
+	   isJump2=false;
+	  }
+	  else { //jeśli nie to w lewo
+	   GetComponent.<Rigidbody2D>().AddForce(Vector2.left*jumpForceSide2);
+	   jumpTimer=0;
+	   isJump2=false;
+	  }
 	}
-	else {
-	 if(jump2Timer>=0.4 && !grounded) {
-	  GetComponent.<Rigidbody2D>().AddForce(Vector2.left *jumpForceSide2);
-	  isJump2=false;
-	  jump2Timer=0;
-	 }
-	}
-	if(jump) {
-	 DblClckTimer+=Time.deltaTime;
+}
+
+//wygrzebywanie się z ziemi
+function OnTriggerStay2D (other:Collider2D) {
+	if(other.CompareTag("atom")) {
+	 GetComponent.<Transform>().position.y+=1;
 	}
 }
 
--- TotallyNotTheWorms/Assets/Skrypty/Generator.js
+++ TotallyNotTheWorms/Assets/Skrypty/Generator.js
@@ -26,7 +26,7 @@ function Start () {
 	h=Random.Range(0,6);
 	n=Random.Range(0,2);
 	//(poziom -> h.n)
-	for (i=0; i<20; i++) {
+	for (i=0; i<38; i++) {
 
 	for (x=0; x<2; x++) {
 	 switch(h) {
