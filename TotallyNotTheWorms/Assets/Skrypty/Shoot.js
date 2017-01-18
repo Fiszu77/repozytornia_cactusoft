ee6ad264de3b797ce65d9a462e913809727c6fae
Kaktus <kaktus500@interia.eu>
2017-01-18 23:31:03 +0100

    Slider działa!!!

--- TotallyNotTheWorms/Assets/Skrypty/Shoot.js
+++ TotallyNotTheWorms/Assets/Skrypty/Shoot.js
@@ -15,36 +15,28 @@ if(GetComponent.<SpriteRenderer>().flipX) {
    //transform.rotation = Quaternion.Euler(rotationVector);
    power.transform.rotation.eulerAngles.y=180;
    arm.transform.rotation.eulerAngles.y=180;
-   //pwr.transform.rotation.eulerAngles.y=180;
+   pwr.transform.rotation.eulerAngles.y=180;
 }
 if(!GetComponent.<SpriteRenderer>().flipX){ 
 //rotationVector.y = 180;
    //transform.rotation = Quaternion.Euler(rotationVector);
     power.transform.rotation.eulerAngles.y=0;
     arm.transform.rotation.eulerAngles.y=0;
-    //pwr.transform.rotation.eulerAngles.y=0;
+    pwr.transform.rotation.eulerAngles.y=0;
 }
 
 
 if(Input.GetKey(KeyCode.LeftShift)){
  if ((power.transform.eulerAngles.z <360 && power.transform.eulerAngles.z > 280)||(power.transform.eulerAngles.z >0  && power.transform.eulerAngles.z < 95)){
   power.transform.Rotate(Vector3.back * Time.deltaTime*50);
- }
- if (arm.transform.eulerAngles.z <360 && arm.transform.eulerAngles.z > 280||arm.transform.eulerAngles.z > 0  && arm.transform.eulerAngles.z < 95){
   arm.transform.Rotate(Vector3.back * Time.deltaTime*50);
- }
- if (pwr.transform.eulerAngles.z <360 && pwr.transform.eulerAngles.z > 280||pwr.transform.eulerAngles.z > 0  && pwr.transform.eulerAngles.z < 95){
   pwr.transform.Rotate(Vector3.back * Time.deltaTime*50);
  }
 }
 if(Input.GetKey(KeyCode.LeftControl)){
  if ((power.transform.eulerAngles.z <360 && power.transform.eulerAngles.z > 275)||(power.transform.eulerAngles.z >0  && power.transform.eulerAngles.z < 90)){
   power.transform.Rotate(Vector3.forward* Time.deltaTime*50);
- }
- if ((arm.transform.eulerAngles.z <360 && arm.transform.eulerAngles.z > 275)||(arm.transform.eulerAngles.z >0  && arm.transform.eulerAngles.z < 90)){
   arm.transform.Rotate(Vector3.forward * Time.deltaTime*50);
- }
- if ((pwr.transform.eulerAngles.z <360 && pwr.transform.eulerAngles.z > 275)||(pwr.transform.eulerAngles.z >0  && pwr.transform.eulerAngles.z < 90)){
   pwr.transform.Rotate(Vector3.forward * Time.deltaTime*50);
  }
 }
@@ -52,7 +44,7 @@ if(Input.GetKey(KeyCode.LeftControl)){
 
 if(Input.GetKey(KeyCode.E)){
 timer+=Time.deltaTime;
-pwrFill.value+=Time.deltaTime*1;
+pwrFill.value+=Time.deltaTime;
 print(timer);
 
 }
