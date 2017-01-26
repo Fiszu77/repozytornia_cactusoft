011daec97af0d89468e2bcac233217e2d505491d
Cactusoft Entertainment <bukaj.i.kaktus@gmail.com>
2017-01-26 22:54:07 +0100

    Pierdoły, pierdółki, menu pauzy, szojda

--- TotallyNotTheWorms/Assets/Skrypty/Character.js
+++ TotallyNotTheWorms/Assets/Skrypty/Character.js
@@ -1,15 +1,26 @@
 ﻿#pragma strict
 
 var inv:GameObject;
+var menu:GameObject;
 var aiming:GameObject[];
 
 function Update () {
 	if(Input.GetKeyDown(KeyCode.Tab)) {
-	 if(inv.active==false) {
+	 if(inv.activeInHierarchy==false) {
 	  inv.SetActive(true);
 	 }
 	 else {
 	  inv.SetActive(false);
+	 }
+	}
+	if(Input.GetKeyDown(KeyCode.Escape)) {
+	 if(menu.activeInHierarchy==false) {
+	  menu.SetActive(true);
+	  Time.timeScale=0;
+	 }
+	 else {
+	  menu.SetActive(false);
+	  Time.timeScale=1;
 	 }
 	}
 }
@@ -29,4 +40,8 @@ function Armed () {
 	aiming[0].SetActive(true);
 	aiming[1].SetActive(true);
 	inv.SetActive(false);
+}
+
+function Resume () {
+	Time.timeScale=1;
 }
---
+++ TotallyNotTheWorms/Assets/Skrypty/Menu.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+import UnityEngine.SceneManagement;
+var exit:boolean=false;
+var normal:boolean=false;
+var toggle:boolean=false;
+var load:boolean=false;
+var fi:GameObject;
+var fi2:GameObject;
+var se:GameObject;
+var th:GameObject;
+var fo:GameObject;
+var play:GameObject;
+var playb:UnityEngine.UI.Button;
+var ptext:GameObject;
+var script:Menu;
+var s:String;
+
+function Click () {
+	if(exit==true) {
+	 Application.Quit();
+	}
+	if(normal==true) {
+	 if(fi.activeInHierarchy==false) {
+	  fi.SetActive(true);
+	  fi2.SetActive(true);
+	 }
+	 if(se.activeInHierarchy==true) {
+	  se.SetActive(false);
+	 }
+	 if(th.activeInHierarchy==true) {
+	  th.SetActive(false);
+	 }
+	 if(fo.activeInHierarchy==true) {
+	  fo.SetActive(false);
+	 }
+	}
+	if(toggle==true) {
+	  playb.interactable=true;
+	  ptext.SetActive(true);
+	  script=play.GetComponent(Menu);
+	  script.s=s;
+	}
+	if(load==true) {
+	 SceneManager.LoadScene(s);
+	}
+}
