5da33626f275dbbe067b8f80e948c81505c86bd1
Cactusoft Entertainment <bukaj.i.kaktus@gmail.com>
2017-01-30 22:00:46 +0100

    Cośtam

--- TotallyNotTheWorms/Assets/Skrypty/Bomb.js
+++ TotallyNotTheWorms/Assets/Skrypty/Bomb.js
@@ -19,3 +19,13 @@ function OnTriggerStay2D (other:Collider2D) {
 	 Destroy(gameObject);
 	}
 }
+
+function OnTriggerEnter2D (other:Collider2D) {
+	if(timer>=5) {
+	 if(other.CompareTag("atom")) {
+	  other.gameObject.SetActive (false);
+	  print("coll");
+	 }
+	 Destroy(gameObject);
+	}
+}
