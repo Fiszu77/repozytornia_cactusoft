7a51582c0c0b51d182944ebcc5ed45f8e948bf46
Cactusoft Entertainment <bukaj.i.kaktus@gmail.com>
2017-03-28 23:08:19 +0200

    Troszku rzeczy, animacja ręki

--- TotallyNotTheWorms/Assets/Skrypty/Character.js
+++ TotallyNotTheWorms/Assets/Skrypty/Character.js
@@ -10,7 +10,7 @@ var hp:float=100;
 var hpSlider:UnityEngine.UI.Slider;
 
 function Update () {
-
+//wł/wył ekwipunku 
 	if(Input.GetKeyDown(KeyCode.Tab)) {
 	 if(inv.activeInHierarchy==false) {
 	  inv.SetActive(true);
@@ -19,6 +19,7 @@ function Update () {
 	  inv.SetActive(false);
 	 }
 	}
+//ał/wył menu
 	if(Input.GetKeyDown(KeyCode.Escape)) {
 	 if(menu.activeInHierarchy==false) {
 	  menu.SetActive(true);
@@ -29,8 +30,8 @@ function Update () {
 	  Time.timeScale=1;
 	 }
 	}
-
-	if(kamera.GetComponent.<Camera>().orthographicSize>=25 && kamera.GetComponent.<Camera>().orthographicSize<=55) {
+//zoom
+	if(kamera.GetComponent.<Camera>().orthographicSize>=10 && kamera.GetComponent.<Camera>().orthographicSize<=55) {
 	 if(Input.GetAxis("Mouse ScrollWheel")>0) {
 	  kamera.GetComponent.<Camera>().orthographicSize-=2;
 	 }
@@ -38,17 +39,17 @@ function Update () {
 	  kamera.GetComponent.<Camera>().orthographicSize+=2;
 	 }
 	}
-	if(kamera.GetComponent.<Camera>().orthographicSize<25) {
-	 kamera.GetComponent.<Camera>().orthographicSize=25;
+	if(kamera.GetComponent.<Camera>().orthographicSize<10) {
+	 kamera.GetComponent.<Camera>().orthographicSize=10;
 	}
 	if(kamera.GetComponent.<Camera>().orthographicSize>55) {
 	 kamera.GetComponent.<Camera>().orthographicSize=55;
 	}
-
+//ustalanie punktów życia
 	Mathf.RoundToInt(hp);
 	hpSlider.value=hp/100;
 }
-
+//wybór broni
 function W0 () {
 	Shoot.weapon=0;
 	Armed();
@@ -62,14 +63,10 @@ function W2 () {
 	Shoot.weapon=2;
 	Armed();
 }
-
+//uzbrojony (broń została wybrana)
 function Armed () {
 	Shoot.isArmed=true;
 	aiming[0].SetActive(true);
 	aiming[1].SetActive(true);
 	inv.SetActive(false);
-}
-
-function Resume () {
-	Time.timeScale=1;
 }
---
+++ TotallyNotTheWorms/Assets/Skrypty/UnBury.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var chara:GameObject;
+
+//wygrzebywanie się z ziemi
+function OnTriggerStay2D (other:Collider2D) {
+	if(other.CompareTag("atom")) {
+	 chara.GetComponent.<Transform>().position.y+=1;
+	}
+}
