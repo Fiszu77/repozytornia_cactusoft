93256a65afc5391a07ec3bd53e169d90d1f5e349
Kaktus <kaktus500@interia.eu>
2017-03-31 23:37:57 +0200

    Coś się zepsuło więc wysyłam jeszcze raz

--- TotallyNotTheWorms/Assets/Skrypty/Rocket.js
+++ TotallyNotTheWorms/Assets/Skrypty/Rocket.js
@@ -5,10 +5,7 @@ var radius:int;
 function Boom () {
 	atomsCollided=Physics2D.OverlapCircleAll(transform.position,radius);
 	for(var i=0;i<atomsCollided.length;i++) {
-<<<<<<< HEAD
 
-=======
->>>>>>> 5226fc42a3ee1ae04a0c7227799bb1f74a3dd084
 	 if(atomsCollided[i].CompareTag("atom")) {
 	  atomsCollided[i].gameObject.SetActive(false);
 	 }
